5a2fb7fdc705c662c3e5430115e648634dd84657
D V <voronov.dmitry@gmail.com>
2020-01-25 23:54:54 -0500

    Created 1st files

---
+++ index.js
@@ -0,0 +1,15 @@
+//Get all of the packages runnung
+const inquire = require("inquirer");
+const axious = require("axios");
+const spotify = require("node-spotify-api");
+const moment = require("moment"); 
+const keyFile = require("./key.js"); 
+require('dotenv').config(); // hide spotify keys
+
+
+//1. Capture request and create a switch statemnt
+
+
+//Run appropriate API
+
+//Return data and write it in a file
