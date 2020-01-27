38d82cd79cd13b099bb536e3a309c27cb37d6a48
D V <voronov.dmitry@gmail.com>
2020-01-27 14:56:43 -0500

    Spotify

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const inquirer = require("inquirer");
 const axios = require("axios");
 const Spotify = require("node-spotify-api");
 const moment = require("moment");
-const keyFile = require("./key.js");
-require('dotenv').config(); // hide spotify keys
+// const keyFile = require("./key.js");
+// require('dotenv').config(); // hide spotify keys
 
 //Global variables go here////////////
 let userChoice; //user category input, see prompt
@@ -30,16 +30,20 @@ inquirer
 
 
         }
-
-
-
     ])
     //-----------------End of rpompts for user choices-------------------////
     .then(function (response) {
+
+        console.log(response.userChoice);
+        console.log(response.userInput);
+        let userChoice = response.userChoice;
+        let userInput = response.userInput;
+
         switch (userChoice) {
             case "Movie":
                 if (userInput != "") {
                     getMovie(userInput);
+                    console.log(response);
                 } else getMovie("Transformers"); //Default movie
 
                 break;
@@ -58,6 +62,7 @@ inquirer
                 } else getConcert("Ramstein"); //Default Concert
 
         }
+        // console.log(response)
     });
 
 
@@ -67,23 +72,43 @@ inquirer
 
 //Spotify API call to the endpoint
 
-const getSong = str => {
+// const getSong = str => {
 
-}
+// }
 //Return data and write it in a
 
 //API call to OMBD
 
-var getMovie = str => {
+var getMovie = (str) => {
 
-    let url = "http://img.omdbapi.com/?apikey=4f53e692&t=${str}"
+    let url = "http://www.omdbapi.com/?apikey=trilogy&t=" + str;
+    console.log(url);
+    // let url = 'http://img.omdbapi.com/?apikey=4f53e692&t=${str}';
+    // let url = `http://img.omdbapi.com/?apikey=4f53e692&t=${str}`;
     axios
         .get(url)
         .then((response) => {
-            console.log(response);
+            console.log(response.data.Title);
+        
         },
             (error) => {
                 console.log(error);
             });
 
 };
+
+var getSong = (str)=> {
+    var spotify = new Spotify({
+        id:"625bbe361130488db88e6dc369d02114",
+        secret: "c508605df74c4d64be80c4546ddb3348"
+      });
+       
+      spotify
+      .search({ type: 'track', query: str })
+      .then(function(response) {
+        console.log(response.items);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+}
